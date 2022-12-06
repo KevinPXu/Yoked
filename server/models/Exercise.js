027fc800275b505b19a2730bcb051796021d5171
josephjensen <joe@cryptonumerics.com>
2022-12-06 11:24:52 -0800

    add bodypart and exercise models

---
+++ server/models/Exercise.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+const BodyPart = require('./BodyPart')
+
+const exerciseSchema = new Schema({
+    exerciseId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    name: {
+        type: String,
+        required: 'Exercise must be named',
+        trim: true
+    },
+    bodyPart: {
+        type: BodyPart,
+        required: true
+    },
+    sets: {
+        type: Number,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+})
+
+const Exercise = model('exercise', exerciseSchema)
+
+module.exports = Exercise;
