85e20ca504b592d6e6b90345db4d88951b88120f
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-06 13:13:20 -0600

    created User model

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        templates: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Template'
+            }
+        ],
+        exercises: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Exercise'
+            }
+        ],
+        bodyParts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'BodyPart'
+            }
+        ],
+        history: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'History'
+            }
+        ],
+        loggedIn: {
+            type: Boolean,
+            required: true
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
