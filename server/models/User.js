a5c41ed9d1e4da273ad4db3421e49800bbc58768
Kevin Xu <kevinpxu21@gmail.com>
2022-12-10 15:52:51 -0600

    added User routes and controller, pretested

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,9 @@ const userSchema = new Schema(
     {
         name: {
             type: String,
-            required: true
+            required: true,
+            unique: true,
+            trim: true,
         },
         templates: [
             {
---
+++ server/routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    login
+} = require('../../controllers/UserController.js');
+
+const { authMiddleware } = require('../../utils/auth');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:id').get(authMiddleware, getSingleUser);
+
+router.route('/login').post(login);
+
+module.exports = router;
