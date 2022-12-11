8d5f30ff49f8d839e5d08a073a8e747bf9fd10c8
joedjensen <47039441+joedjensen@users.noreply.github.com>
2022-12-11 11:32:19 -0800

    Update index.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -5,7 +5,7 @@ const bodyPartRoutes = require('./bodyPartRoutes')
 
 
 router.use('/user', userRoutes);
-router.use('./template', templateRoutes);
+router.use('/template', templateRoutes);
 router.use('/bodyparts', bodyPartRoutes)
 
 module.exports = router;
