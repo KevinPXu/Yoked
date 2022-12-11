846f45527afb6d51083b035767672a391d7a8c1d
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-11 11:27:59 -0600

    fixed saveHistory route

--- server/controllers/HistoryController.js
+++ server/controllers/HistoryController.js
@@ -1,6 +1,7 @@
 const { History, User } = require('../models/');
 
 module.exports = {
+    // GET to /users/:userId/history
     async getHistory(req, res) {
         const userHistory = await User.find({ _id: req.params.userId })
             .populate({
@@ -15,17 +16,20 @@ module.exports = {
             : res.json(userHistory);
      },
 
+    // POST to /users/:userId/history
     async saveToHistory(req, res) {
+        const newHistory = await History.create(req.body);
         const updatedUser = await User.findOneAndUpdate(
             { _id: req.params.userId },
-            { $push: { history: req.body } },
-            { runValidators: true, new: true }
+            { $push: { history: newHistory.historyId } },
+            { new: true }
         );
         !updatedUser 
             ? res.status(404).json({ message: `Couldn't find a user with that ID!` })
             : res.json(updatedUser);
     },
 
+    // GET to /users/:userId/history/:historyId
     async getSingleHistory(req, res) {
         const history = await History.findOne({ historyId: req.params.historyId })
             .populate('exercises');
