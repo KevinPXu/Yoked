cdc3a705eefe71dbeca4158129fa5fc1a5467485
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-11 13:01:08 -0600

    updated HistoryController + routes to remove :userId from route params

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -21,8 +21,8 @@ router.route('/:id').get(authMiddleware, getSingleUser);
 
 router.route('/login').post(login);
 
-router.route('/:userId/history').get(authMiddleware, getHistory).post(authMiddleware, saveToHistory);
+router.route('/history').get(authMiddleware, getHistory).post(authMiddleware, saveToHistory);
 
-router.route('/:userId/history/:historyId').get(authMiddleware, getSingleHistory);
+router.route('/history/:historyId').get(authMiddleware, getSingleHistory);
 
 module.exports = router;
