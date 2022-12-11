f7cdda37a74c312d2293739754863627c6eecd79
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-11 11:05:53 -0600

    nested subdocs now populate

--- server/controllers/HistoryController.js
+++ server/controllers/HistoryController.js
@@ -1,16 +1,20 @@
 const { History, User } = require('../models/');
 
 module.exports = {
-    // route probably needs to be /users/:userId/history
     async getHistory(req, res) {
         const userHistory = await User.find({ _id: req.params.userId })
-            .populate('history');
+            .populate({
+                path: 'history',
+                populate: {
+                    path: 'exercises',
+                    model: 'exercise'
+                }
+            });
         !history
             ? res.status(404).json({ message: `Couldn't find a user with that ID!`})
             : res.json(userHistory);
      },
 
-    // POST to /users/:userId/history
     async saveToHistory(req, res) {
         const updatedUser = await User.findOneAndUpdate(
             { _id: req.params.userId },
@@ -22,9 +26,9 @@ module.exports = {
             : res.json(updatedUser);
     },
 
-    // /users/:userId/history/:historyId
     async getSingleHistory(req, res) {
-        const history = await History.findOne({ historyId: req.params.historyId });
+        const history = await History.findOne({ historyId: req.params.historyId })
+            .populate('exercises');
         !history
             ? res.status(404).json({ message: `Couldn't find a history with that ID!`})
             : res.json(history);
