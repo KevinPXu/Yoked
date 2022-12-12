43ed7195e15318130c5f03db9592161586ad103b
josephjensen <joe@cryptonumerics.com>
2022-12-12 13:30:28 -0800

    add simple page routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,29 @@
 // const App = () => <PortfolioContainer />;
 
 // export default App;
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login'
+import UserHistory from './pages/UserHistory'
 
 function App() {
-    return (
-        <main className="App">
-            <Login />
-        </main>
-    );
+  return (
+    <Router>
+            <main className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={<Login />}
+        />
+        <Route
+          path="/history"
+          element={<UserHistory />}
+        />
+      </Routes>
+
+
+      </main>
+    </Router>
+  );
 };
 
 export default App;
