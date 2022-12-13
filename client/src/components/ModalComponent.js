758cc5bce54a44cfc9dcbbe40cf452e1fecf6357
Kevin Xu <kevinpxu21@gmail.com>
2022-12-13 15:30:22 -0600

    added some functionality for temp buttons, waiting on model refactor

--- client/src/components/ModalComponent.js
+++ client/src/components/ModalComponent.js
@@ -4,22 +4,7 @@ import { Button, Dialog, DialogTitle, DialogContent } from '@mui/material';
 
 function ModalComponent({ closeTempModal, handleTempClose }) {
   const [exerciseList, setExerciseList] = useState([]);
-  // const style = {
-  //   display: 'flex',
-  //   flexDirection: 'column',
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   position: 'absolute',
-  //   top: '50%',
-  //   left: '50%',
-  //   color: 'black',
-  //   transform: 'translate(-50%, -50%)',
-  //   width: 400,
-  //   bgcolor: 'background.paper',
-  //   border: '2px solid #000',
-  //   boxShadow: 24,
-  //   p: 4,
-  // };
+
   const handleAddExBtn = () => {
     setExerciseList(exerciseList.concat(<ExerciseComponent />));
   };
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,7 +3,10 @@ import { gql } from '@apollo/client';
 export const QUERY_TEMP = gql`
   query templates {
     templates {
-      name
+      exercises {
+        _id
+        name
+      }
     }
   }
 `;
