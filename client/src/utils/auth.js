6ae433513b2784e5e7637aba99bfeb6e5059ca4f
Kevin Xu <kevinpxu21@gmail.com>
2022-12-15 13:23:51 -0600

    deploy

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,41 +1,41 @@
 import decode from 'jwt-decode';
 
 class AuthService {
-    loggedIn() {
-        const token = this.getToken();
-        return !!token && !this.isTokenExpired(token);
-    }
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
 
-    getProfile() {
-        return decode(this.getToken());
-    }
-    
-    isTokenExpired(token) {
-        try {
-            const decoded = decode(token);
-            if(decoded.exp < Date.now() / 1000) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (err) {
-            return false;
-        }
-    }
+  getProfile() {
+    return decode(this.getToken());
+  }
 
-    getToken() {
-        return localStorage.getItem('id_token');
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      return false;
     }
+  }
 
-    login(idToken) {
-        localStorage.setItem('id_token', idToken);
-        window.location.assign('/templates');
-    }
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
 
-    logout() {
-        localStorage.removeItem('id_token');
-        window.location.assign('/');
-    }
+  login(idToken) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
+  }
 }
 
 export default new AuthService();
